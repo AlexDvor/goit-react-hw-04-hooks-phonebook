24d01f4c504282d0d0bcb09ad4842e3573b66155
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-17 18:07:37 +0200

    bug-fix state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Filter from './components/Filter';
 
 export default function App() {
   const [contacts, setContacts] = useState(
-    () => JSON.parse(window.localStorage.getItem('contacts')) ?? '',
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? [],
   );
   const [filter, setFilter] = useState('');
 
